a5cc1a065a875532d53f7c7e7c88440535e4a4c6
N3X15 <nexisentertainment@gmail.com>
2011-03-27 20:39:07 -0700

    Flatgrass stuff.

--- src/main/java/net/nexisonline/spade/chunkproviders/ChunkProviderFlatGrass.java
+++ src/main/java/net/nexisonline/spade/chunkproviders/ChunkProviderFlatGrass.java
@@ -1,44 +1,35 @@
 package net.nexisonline.spade.chunkproviders;
 
-import java.util.Random;
 import java.util.logging.Logger;
 
-import net.minecraft.server.NoiseGeneratorOctaves;
-
 import org.bukkit.ChunkProvider;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
-import org.bukkit.block.Block;
 
 public class ChunkProviderFlatGrass extends ChunkProvider {
-	
-	private Random random;
-	//private World world;
-	private NoiseGeneratorOctaves hillnoise;
 
 	public void onLoad(World world, long seed) {
-		//this.world=world;
-		this.random = new Random(seed);
-		this.hillnoise = new net.minecraft.server.NoiseGeneratorOctaves(random, 4);
+		
 	}
 
 	@Override
-	public void generateChunk(World world, int X, int Z, byte[] abyte, Biome[] biomes,
-			double[] temperature) {
-		for(int x = 0;x<16;x++) {
-			for(int z=0;z<16;z++) {
-				double h = hillnoise.a((X*16)+x, (Z*16)+z)/10; // Yes we're doing a heightmap
-				for(int y=0;y<h+63;y++) {
-
-					abyte[getBlockIndex(x,y,z)]=(byte) ((y<2) ? Material.BEDROCK.getId() : 1);
+	public void generateChunk(World world, int X, int Z, byte[] abyte,
+			Biome[] biomes, double[] temperature) {
+		for (int x = 0; x < 16; x++) {
+			for (int z = 0; z < 16; z++) {
+				double h = 63;
+				for (int y = 0; y < h + 63; y++) {
+					abyte[getBlockIndex(x, y, z)] = (byte) ((y < 2) ? 7 : 1);
 				}
 			}
 		}
-		Logger.getLogger("Minecraft").info(String.format("Chunk (%d,%d)",X,Z));
+		Logger.getLogger("Minecraft")
+				.info(String.format("Chunk (%d,%d)", X, Z));
 	}
+
 	@Override
-	public void populateChunk(World world, int x, int z, byte[] abyte, Biome[] biomes) {
+	public void populateChunk(World world, int x, int z, byte[] abyte,
+			Biome[] biomes) {
 		// TODO Auto-generated method stub
 
 	}
@@ -64,7 +55,7 @@ public class ChunkProviderFlatGrass extends ChunkProvider {
 	@Override
 	public void generateCaves(World world, int x, int z, byte[] abyte) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
