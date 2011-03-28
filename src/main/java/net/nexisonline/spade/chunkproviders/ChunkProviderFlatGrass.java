41374eee806dad5fc8754526be789a583d412e86
N3X15 <nexisentertainment@gmail.com>
2011-03-28 16:29:43 -0700

    Trying to fix flatgrass. :|

--- src/main/java/net/nexisonline/spade/chunkproviders/ChunkProviderFlatGrass.java
+++ src/main/java/net/nexisonline/spade/chunkproviders/ChunkProviderFlatGrass.java
@@ -26,6 +26,16 @@ public class ChunkProviderFlatGrass extends ChunkProvider {
 	public void generateChunk(World world, int X, int Z, byte[] abyte,
 			Biome[] biomes, double[] temperature) {
 		abyte=template;
+		if(X==0&&Z==0) {
+			int z=0;
+			int x=0;
+			for (int y = 0; y < 64; y++) {
+				// Origin point + sand to prevent 5000 years of loading.
+				if(x==0&&z==0&&X==x&&Z==z&&y<=63)
+					abyte[getBlockIndex(x, y, z)]=(byte) ((y==63)?12:7);
+			}
+
+		}
 		Logger.getLogger("Minecraft")
 				.info(String.format("[Flatgrass] Chunk (%d,%d)", X, Z));
 	}
