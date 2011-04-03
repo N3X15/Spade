9a2e3914b1e6c132121d3625e26c9b1fef091471
N3X15 <nexisentertainment@gmail.com>
2011-04-03 10:29:24 -0700

    Completing density map interpolator, maybe.

--- src/main/java/net/nexisonline/spade/Densitymap.java
+++ src/main/java/net/nexisonline/spade/Densitymap.java
@@ -12,10 +12,16 @@ public class Densitymap {
 		}
 		
 		public void set(int x,int y,int z,double h) {
-			map[(x & 0xF)][(z & 0xF)][(y & 0x7F)]=h;
+			if (x < 0 || x >= width) x = (width + x) & (width - 1);
+			if (y < 0 || y >= depth) y = (depth + y) & (depth - 1);
+			if (z < 0 || z >= height) z = (height + z) & (height - 1);
+			map[x][y][z]=h;
 		}
 		
 		public double get(int x,int y,int z) {
-			return map[(x & 0xF)][(z & 0xF)][(y & 0x7F)];
+			if (x < 0 || x >= width) x = (width + x) & (width - 1);
+			if (y < 0 || y >= depth) y = (depth + y) & (depth - 1);
+			if (z < 0 || z >= height) z = (height + z) & (height - 1);
+			return map[x][y][z];
 		}
 }
