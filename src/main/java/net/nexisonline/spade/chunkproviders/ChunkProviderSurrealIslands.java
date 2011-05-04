85fa44b1007c3b1ad7380d797a1477ec46d04f1d
N3X15 <nexis@7chan.org>
2011-05-04 15:48:02 -0700

    Forgot sum loopz

--- src/main/java/net/nexisonline/spade/chunkproviders/ChunkProviderSurrealIslands.java
+++ src/main/java/net/nexisonline/spade/chunkproviders/ChunkProviderSurrealIslands.java
@@ -163,9 +163,9 @@ public class ChunkProviderSurrealIslands extends SpadeChunkProvider {
 		 */
 		double frequency = 0;
 		double amplitude = 0;
-		for (int x = 0; x < 16; x++) {
+		for (int x = 0; x < 16; x+=5) {
 			for (int y = 0; y < 128; y += 16) {
-				for (int z = 0; z < 16; z++) {
+				for (int z = 0; z < 16; z+=5) {
 					double posX = (x + (X * 16));
 					double posY = (y - 64);
 					double posZ = (z + (Z * 16));
