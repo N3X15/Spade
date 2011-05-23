9bf840ed1a3f0e031d6ad676ced61e0529b1088e
N3X15 <nexis@7chan.org>
2011-05-23 01:51:53 -0700

    Revert stalactites

--- src/main/java/net/nexisonline/spade/generators/StalactiteGenerator.java
+++ src/main/java/net/nexisonline/spade/generators/StalactiteGenerator.java
@@ -31,8 +31,9 @@ public class StalactiteGenerator extends SpadeEffectGenerator {
 
 	private void addStalactite(World w,int x, int z) {
 		for(int y = 1;y<127;y++) {
-			if(blockIsCeiling(get(x,y,z)) && (!blockIsCeiling(get(x,y-1,z)))) {
+			if(get(x,y,z)==1 && (get(x,y-1,z)==0 || get(x,y-1,z)==8)) {
 				int h=rnd.nextInt(15);
+				//for(;!(get(x,y-h,z)==1)&&y-h>1;h++) {}
 				y-=h;
 				addStalactite(w,x+(X*16), y, x+(Z*16));
 			}
@@ -49,48 +50,16 @@ public class StalactiteGenerator extends SpadeEffectGenerator {
 		boolean E=false;
 		boolean W=false;
 		boolean S=false;
-		
-		// Dry run to make sure we won't be making columns.
-		for(int y2=y;y2<128&&!blockIsCeiling(w.getBlockAt(x,y2,z).getTypeId());y2++) {
-			if(y2>111) {
-				return;
-			}
-		}
-		
-		for(;y<128&&!blockIsCeiling(w.getBlockAt(x,y,z).getTypeId());y++) {
+		for(;y<128&&w.getBlockAt(x,y,z).getTypeId()==0;y++) {
 			w.getBlockAt(x,y,z).setTypeId(1);
-			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !N) {
+			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !N)
 				addStalactite(w,x+1,y,z);
-				N=true;
-			}
-			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !E) {
+			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !E)
 				addStalactite(w,x,y,z+1);
-				E=true;
-			}
-			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !W) {
+			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !W)
 				addStalactite(w,x-1,y,z);
-				W=true;
-			}
-			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !S) {
+			if(rnd.nextDouble()<MINISTALACTITE_CHANCE && !S)
 				addStalactite(w,x,y,z-1);
-				S=true;
-			}
-		}
-	}
-
-	private boolean blockIsCeiling(int typeId) {
-		switch(typeId) {
-		case 0: // Air
-		case 8: // Water
-		case 9: // Water
-		case 10:// Lava
-		case 11:// Lava
-		case 17:// Wood
-		case 18:// Leaves
-		case 48:// 
-			return false;
-		default:
-			return true;
 		}
 	}
 
